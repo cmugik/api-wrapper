587d9d47c16e074691f2f2648de5691e3ffdb5f6
cmugik <caleb.kellett@gmail.com>
2024-05-03 16:34:31 -0700

    replace boilerplate in app/main/router
    add main app layout/structure in App.vue
    add mounting of main app in main
    remove boilerplate from router index.ts

--- src/FrontendService/FrontendVue/src/App.vue
+++ src/FrontendService/FrontendVue/src/App.vue
@@ -1,85 +1,66 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import ConvoDisplay from './components/ConvoDisplay.vue';
+import ModelConvoDisplay from './components/ModelConvoDisplay.vue';
+import UserInputDisplay from './components/UserInputDisplay.vue'
+import { ref, type Ref } from 'vue'
+
+const selectedConversationId: Ref<number | null> = ref<number|null>(null);
+
+const handleConversationSelect = (convoId: number) => {
+  selectedConversationId.value = convoId;
+};
+
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="container">
+    <div class="left-column">
+      <div class="convo-frame">
+        <ConvoDisplay :convo-id="selectedConversationId"/>  
+      </div>
+      <div class="user-input-frame">
+        <UserInputDisplay />
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="model-convo-frame">
+      <ModelConvoDisplay @convo-selected="handleConversationSelect"/>
+    </div>
+  </div>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+html, body {
+  height: 100%;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  margin: 0;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.container {
+  display:flex;
+  flex-direction: row;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.left-column {
+  display:flex;
+  flex-direction: column;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.convo-frame{
+  display: flex;
+  overflow: auto;
+  flex-direction: column-reverse;
+  justify-items: flex-start;
+  align-items: start;
+  width: 1200px;
+  height: 832px;
+  border: 2px solid #ccc;
+  background-color: var(--surface-ground);
 }
-
-nav a:first-of-type {
-  border: 0;
+.user-input-frame {
+  width: 1200px;
+  height: 192px;
+  border: 2px solid #ccc; 
+  background-color: var(--surface-ground);
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.model-convo-frame {
+  width: 348px;
+  height:1024px;
+  border: 2px solid #ccc;
+  background-color: var(--surface-ground);
 }
 </style>
--- src/FrontendService/FrontendVue/src/main.ts
+++ src/FrontendService/FrontendVue/src/main.ts
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+//import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+// Import PrimeVue components and CSS
+import PrimeVue from 'primevue/config'; 
+import 'primevue/resources/themes/luna-amber/theme.css';  
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+//app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, { inputStyle: "filled" }); 
+app.mount('#app');
