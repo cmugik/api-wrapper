b1d1428b38e387a298c758ab0f0228b583c78371
cmugik <caleb.kellett@gmail.com>
2024-05-03 16:37:02 -0700

    add global type to frontend

---
+++ src/FrontendService/FrontendVue/src/global.d.ts
@@ -0,0 +1,21 @@
+export { Message, Conversation };
+
+declare global {
+
+    interface Message {
+        role: 'user' | 'system' | 'assistant';
+        content: string;
+        name?: string; 
+        severity?: 'warning' | 'error';
+        date: Date;
+        id: number; 
+        conversationId: number;
+    }
+
+    interface Conversation {
+        id: number;
+        name: string;
+        createdAt: Date;
+    }
+
+}
